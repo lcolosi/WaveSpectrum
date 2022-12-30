e777934144e43b2431410c9af7e7df5af3eb8daf
Luke Colosi <lcolosi@ucsd.edu>
2022-12-30 12:32:24 -0800

    Set up planes where specific cases will considered in subplots.

--- src/figA1.m
+++ src/figA1.m
@@ -20,18 +20,18 @@ fig_path = '../figs/';
 % Set physical parameters
 g = 9.81;                                                                   % Gravitational acceleration (units: m/s^2)
 H = 4000;                                                                   % Water depth (units: m)
-U = 0.55;                                                                    % Platform speed (units: m/s)
+U = 0.55;                                                                   % Platform speed (units: m/s)
 phi = 0;                                                                    % Platform propagation direction (units: degrees)
 
 % Set wave parameters
 f_ob = 0.5;                                                                 % Observed cyclicaly frequency (units: Hz)
 omega = 2*pi*f_ob;                                                          % Observed radian frequency (units: rad/s)
-k = -50:0.5:50;                                                               % x-component of wave number vector (units: rad/s)
-l = -50:0.5:50;                                                               % y-component of wave number vector (units: rad/s)
+k = -50:0.5:50;                                                             % x-component of wave number vector (units: rad/s)
+l = -50:0.5:50;                                                             % y-component of wave number vector (units: rad/s)
 [k_m, l_m] = meshgrid(k,l); 
 
 % Compute wavenumber magnitude and direction
-theta = atan2d(l_m,k_m);                                                     % Wave propagation direction (units: degrees)
+theta = atan2d(l_m,k_m);                                                    % Wave propagation direction (units: degrees)
 K = sqrt(k_m.^2 + l_m.^2);                                                  % Wavenumber magnitude (units: rad/m)
 
 % Compute the relative direction between the platform and waves
@@ -45,9 +45,9 @@ sigma_neg = -(g*K.*tanh(K*H)).^(1/2);
 m = omega + K.*U.*cosd(theta_r); 
 
 % Compute plane for threes relative heading cases
-plane_w = 
-plane_a = 
-plane_n = 
+% plane_w = 
+% plane_a = 
+% plane_n = 
 
 %% Plot dispersion relation surfaces 
 
@@ -71,7 +71,7 @@ sc1 = surf(k,l,sigma_pos);
 hold on 
     sc2 = surf(k,l,sigma_neg);
     sc3 = surf(k,l,m);
-    sc4 = surf(k,l,)
+    % sc4 = surf(k,l,)
 hold off
 
 % Set figure attributes
@@ -84,5 +84,6 @@ sc3.FaceColor = green;
 xlabel('k (rad $\cdot$ m$^{-1}$)', 'Position', [23.991946187616122,-32.11125744837864,-37.56729252760999])
 ylabel('l (rad $\cdot$ m$^{-1}$)', 'Position', [-41.00431899502064,22.52488724201885,-36.405558933024224])
 zlabel('$\sigma$ or $\omega + \textbf{k}\cdot\textbf{U}$  (rad $\cdot$ s$^{-1}$)')
+zlim([-40 40])
 % set(gca,'ZScale','log')
 
