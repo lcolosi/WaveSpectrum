209bb15f80cb8aae3df70d61a616ed33f47860e3
Luke Colosi <lcolosi@ucsd.edu>
2023-02-08 08:09:02 -0800

    Remove unnecessary function arguments, update variance calulation, make code consistent with methology in paper.

--- tools/map_omni_dir_spectrum.m
+++ tools/map_omni_dir_spectrum.m
@@ -1,33 +1,34 @@
-function [S_fin, f_int, S_fob, f_obs, f_b, df_int, J, variance] = map_omni_dir_spectrum(S_fob, f_obs, f_cut, df_obs, U, theta_r, tail)
+function [S_fin, f_int, f_b, variance] = map_omni_dir_spectrum(S_fob, f_obs, f_cut, U, theta_r, tail)
     
     %%%%
-    % [S_fin, f_int, S_fob, f_obs, f_b, df_int, J, variance] = map_omni_dir_spectrum(S_fob, f_obs, f_cut, df_obs, U, theta_r, tail)
+    % [S_fin, f_int, f_b, variance] = map_omni_dir_spectrum(S_fob, f_obs, f_cut, U, theta_r, tail)
     %
     % Function for mapping observed frequency (frequency measured in the 
     % moving reference frame of the platform) to intrinsic frequency 
     % (frequency measured in the absence of platform motion) for deep water
-    % surface gravity waves. Function also transforms the omni-directional 
-    % wave power spectrum, which is a function of observed frequency
-    % S(f_in) to a power spectrum as a function of intrinsic frequency.  
+    % surface gravity waves. Function also maps the observed 1D spectrum 
+    % S_ob(f_ob) (i.e., the omni-direction frequency spectrum) into 
+    % intrinsic frequency space as the intrinsic 1D spectrum S_in(f_in). 
+    % The observed frequency spectrum may be computed by either integrating
+    % the observed 2D spectrum over all directions or computing the 
+    % frequency spectrum of the platform s vertical displacement using the
+    % Welch method   
     %
     %   Parameters
     %   ----------
-    %   S_fob : Wave power spectrum as function of observed frequency 
-    %           S(f_obs). Defined nomially as the power spectrum of vertical
-    %           displacement or heave of a platform. Units: m^2 Hz^(-1).
-    %   f_obs : Observed Frequency (i.e. frequency measured in the 
-    %           moving reference frame of the platform). Units: Hz. 
+    %   S_fob : Observed 1D spectrum S(f_obs). Units: m^2 Hz^(-1).
+    %   f_obs : Observed Frequency. Units: Hz. 
     %   f_cut : Noise cutoff frequency for the platform. For the SV3 wave
     %           gliders, f_cut is nominally ~1 Hz (corresponds to waves 
     %           with a wavelength half the length of the waveglider).
     %           Units Hz. 
-    %   df_obs : Observed frequency resolution of input spectrum. Units: Hz. 
     %   U : Propagation speed of the observer.  Units: ms^(-1). 
     %   theta_r : Angle between the propagation velocity of the platform
-    %             and the direction of propagation of waves. This is
-    %             prescribed as a scalar value such that the mean wave
-    %             direction is assumed to be aligned with the mean wind 
-    %             direction for locally forced waves. Units: Degrees.
+    %             and the direction of propagation of waves. Here, this is
+    %             scalar value such that the wave field is assumed to be 
+    %             uni-directional. The wave direction may assumed to be 
+    %             aligned with the mean wind direction. However, other wave
+    %             direction may be used. Units: Degrees.
     %   tail : Specifies whether a spectral tail with a given slope is 
     %          added to the intrinsic frequency spectrum after the 
     %          blocking frequency (if the f_b < f_cut). Options include:
@@ -43,52 +44,55 @@ function [S_fin, f_int, S_fob, f_obs, f_b, df_int, J, variance] = map_omni_dir_s
     % 
     %   Returns
     %   -------
-    %   S_fin : Wave power spectrum as a function of intrinsic frequency. 
-    %           This quantity is multiplied by the jacobian. 
-    %           Units: m^2 Hz^(-1).
+    %   S_fin : Intrinsic 1D spectrum. Units: m^2 Hz^(-1).
     %   f_int : Intrinsic Frequency. Units: Hz.
-    %   S_fob : Wave power spectrum as a function of observed frequency.
-    %          This quantity is the same as the input. Units: m^2 Hz^(-1).
-    %   f_obs : Observed Frequency. Units: Hz.
     %   f_b : Bifurcation frequency and last frequency not an NaN (blocking 
     %         frequency). The bifircation frequency exists for branches 3-5
     %         (platform moving in direction of wave propagation) and is NaN
-    %         for branches 1-2. The bocking frequency is set by the 
-    %         interpolation and the bifurcation freq. Bifurcation frequency
-    %         is saved in first column f_bif = f_b(:,1) and blocking 
-    %         frequency is saved in second column f_block = f_b(:,2). 
-    %         Units: Hz.
-    %   df_int : Differential for intrinsic frequency. Units: Hz. 
-    %   J : Jacobian. Units: unitless. 
+    %         for branches 1-2. The bocking frequency is set by the
+    %         jacobian, the linear interpolation and the bifurcation
+    %         frequency. The Bifurcation frequency is saved in first column
+    %         f_bifurcation = f_b(:,1) and blocking frequency is saved in
+    %         second column f_block = f_b(:,2). Units: Hz.
     %   variance : Matrix containing four values for the variance computed
     %              over a given frequency band using rectangular (Riemann) 
-    %             integration method: 
+    %              integration method: 
     %                   variance = [f_ob_0.01, f_in_0.01, f_ob_0.1, f_in_0.1]
     %             where: 
     %
     %             (1) f_ob_0.01 : Variance computed from integrating 
-    %                             S(f_obs)*df_obs from 0.01 to 1 Hz
+    %                             S_ob(f_ob)*df_ob from 0.01 to 1 Hz
     %             (2) f_in_0.01 : Total variance computed from integrating
-    %                             S(f_int)*df_obs/df_int*df_int from 0.01 to 1 Hz
+    %                             S_in(f_in)*df_in from
+    %                             f_in(f_ob = 0.01) to f_in(f_ob = 1) Hz
+    %                             (head-seas case) and 1 Hz for
+    %                             (following-seas case)
     %             (3) f_ob_0.1 : Variance computed from integrating 
-    %                            S(f_obs)*df_obs from 0.1 to 1 Hz
+    %                            S_ob(f_ob)*df_ob from 0.1 to 1 Hz
     %             (4) f_in_0.1 : Variance computed from integrating
-    %                            S(f_int)*df_obs/df_int*df_int from 0.1 to 1 Hz
+    %                            S_in(f_in)*df_in from f_in(f_ob = 0.1) to 
+    %                            f_in(f_ob = 1) Hz (head-seas case) and 
+    %                            1 Hz for (following-seas case) 
     %
-    % Notes
-    % -----
-    % (1) Update fitting spectral tail code. Remove all directional spectra
-    %     specific code (i.e. iangle, etc.). 
-    % (2) Update variance calculation. 
+    %   Notes
+    %   -----
+    %   (1) Update variance calculation to include correct limits of
+    %       integration. 
     %%%%
     
     % Set constants
-    g = 9.81;  % Gravitational Acceleration (units: ms^(-2))  
+    g = 9.81;                                                               % Gravitational Acceleration (units: ms^(-2))  
+    df_obs = unique(round(diff(f_obs),4));                                  % Observed frequency resolution of input spectrum (units: Hz)
+    f_obs_li = 0.01; f_obs_lf = 1;                                          % Limits of integration for the lower frequency domain integration (units: Hz)
+    f_obs_hi = 0.1; f_obs_hf = 1;                                           % Limits of integration for the higher frequency domain integration (units: Hz)
     
     % Initialize variables
     S_fin = NaN(size(S_fob));
     f_b = NaN(1,2);
     
+    % Compute the Doppler shift speed projected onto the ith wave direction
+    c_p = U*cosd(theta_r); 
+
     %------- Branch Conditional statements -------%
 
     %---------- Branch 1 : Moving against waves ----------%
@@ -96,7 +100,13 @@ function [S_fin, f_int, S_fob, f_obs, f_b, df_int, J, variance] = map_omni_dir_s
         %disp('Branch 1 : Moving against waves');
 
         % Compute intrinsic frequency for branch 1
-        f_int = f_obs + (g - 4*pi*f_obs*U*cosd(theta_r) - sqrt(g^2 - 8*pi*f_obs*g*U*cosd(theta_r)))/(4*pi* U * cosd(theta_r));
+        f_int = (g - sqrt(g^2 - 8*pi*f_obs*g*c_p))/(4*pi*c_p);
+
+        % Compute intrinsic frequency limits of integration
+        f_int_li = (g - sqrt(g^2 - 8*pi*f_obs_li*g*c_p))/(4*pi*c_p);
+        f_int_lf = (g - sqrt(g^2 - 8*pi*f_obs_lf*g*c_p))/(4*pi*c_p);
+        f_int_hi = (g - sqrt(g^2 - 8*pi*f_obs_hi*g*c_p))/(4*pi*c_p);
+        f_int_hf = (g - sqrt(g^2 - 8*pi*f_obs_hi*g*c_p))/(4*pi*c_p);
 
     %---------- Branch 2 : Moving noraml to waves ----------%
     elseif cosd(theta_r) == 0 || U == 0
@@ -104,54 +114,72 @@ function [S_fin, f_int, S_fob, f_obs, f_b, df_int, J, variance] = map_omni_dir_s
         
         % Compute intrinsic frequency for branch 2 
         f_int = f_obs; 
+
+        % Compute intrinsic frequency limits of integration
+        f_int_li = f_obs_li;
+        f_int_lf = f_obs_lf;
+        f_int_hi = f_obs_hi;
+        f_int_hf = f_obs_hf;
         
     %---------- Branch 3-5 : Moving with wave ----------%
     elseif cosd(theta_r) > 0 && U > 0
         %disp('Branch 3-5 : Moving with wave');
 
         % Compute f_obs where df_in(f_obs)/df_ob tends towards infinity  
-        f_ob_max = (g)/(8*pi*U*cosd(theta_r));
+        f_ob_max = (g)/(8*pi*c_p);
 
         % Compute f_int at the f_obs value where df_in(f_obs)/df_ob
         % tends towards infinity and save in f_b array
-        f_in_max = (g)/(4*pi*U*cosd(theta_r));
+        f_in_max = (g)/(4*pi*c_p);
         f_b(1,1) = f_in_max; 
         
         % Compute f_int when f_obs = 0
-        f_in_zero = (g)/(2*pi*U*cosd(theta_r));
+        f_in_zero = (g)/(2*pi*c_p);
 
         %---------- Platform moving slower than energy and crests ----------%
         if f_in_max > f_cut
             % disp('moving slower than energy and crests');
             
             % Compute the f_in value where f_int = f_cut for branch 3
-            f_ob_cut = -((2*pi*U*cosd(theta_r)*f_cut^2)/(g)) + f_cut;
+            f_ob_cut = -((2*pi*c_p*f_cut^2)/(g)) + f_cut;
 
             % Obtain f_obs values that map to f_int less than or equal to f_ob_cut
             f_obs_map = f_obs(f_obs <= f_ob_cut);
 
-            % Compute total frequency 
-            f_int_s = f_obs_map + (g - 4*pi*f_obs_map*U*cosd(theta_r) - sqrt(g^2 - 8*pi*f_obs_map*g*U*cosd(theta_r)))/(4*pi* U * cosd(theta_r));
+            % Compute intrinsic frequency 
+            f_int_s = (g - sqrt(g^2 - 8*pi*f_obs_map*g*c_p))/(4*pi*c_p);
 
             % Insert NaNs to extend the length of f_int_s to the length of
             % f_obs
             nNaNs = length(f_obs) - length(f_int_s); 
             f_int = [f_int_s, nan(nNaNs, 1)'];
 
+            % Compute intrinsic frequency limits of integration
+            f_int_li = (g - sqrt(g^2 - 8*pi*f_obs_li*g*c_p))/(4*pi*c_p);
+            f_int_lf = f_obs_lf;
+            f_int_hi = (g - sqrt(g^2 - 8*pi*f_obs_hi*g*c_p))/(4*pi*c_p);
+            f_int_hf = f_obs_hf;
+
         %---------- Platform moving faster than energy but slower than crests ----------%
-        elseif f_tot_max  > 0 && f_tot_max < f_cut && f_in_zero > f_cut
+        elseif f_in_max  > 0 && f_in_max < f_cut && f_in_zero > f_cut
             % disp('moving faster than energy but slower than crests');
             
             % Obtain f_obs values for mapping to branch 3 (f_obs less than or equal to f_ob_max)
             f_obs_map = f_obs(f_obs <= f_ob_max);
 
             % Compute intrinsic frequency for branch 3
-            f_int_s = f_obs_map + (g - 4*pi*f_obs_map*U*cosd(theta_r) - sqrt(g^2 - 8*pi*f_obs_map*g*U*cosd(theta_r)))/(4*pi* U * cosd(theta_r));
+            f_int_s = (g - sqrt(g^2 - 8*pi*f_obs_map*g*c_p))/(4*pi*c_p);
             
             % Insert NaNs to extend the length of f_int to the length of
             % f_in
             nNaNs = length(f_obs) - length(f_int_s); 
             f_int = [f_int_s, nan(nNaNs, 1)'];
+
+            % Compute intrinsic frequency limits of integration
+            f_int_li = (g - sqrt(g^2 - 8*pi*f_obs_li*g*c_p))/(4*pi*c_p);
+            f_int_lf = f_obs_lf;
+            f_int_hi = (g - sqrt(g^2 - 8*pi*f_obs_hi*g*c_p))/(4*pi*c_p);
+            f_int_hf = f_obs_hf;
 
         %---------- Platform moving faster than energy and crests ----------%
         elseif f_in_zero < f_cut
@@ -161,17 +189,23 @@ function [S_fin, f_int, S_fob, f_obs, f_b, df_int, J, variance] = map_omni_dir_s
             f_obs_map = f_obs(f_obs <= f_ob_max);
 
             % Compute intrinsic frequency for branch 3
-            f_int_s = f_obs_map + (g - 4*pi*f_obs_map*U*cosd(theta_r) - sqrt(g^2 - 8*pi*f_obs_map*g*U*cosd(theta_r)))/(4*pi*U * cosd(theta_r));
+            f_int_s = (g - sqrt(g^2 - 8*pi*f_obs_map*g*c_p))/(4*pi*c_p);
             
-            % Insert NaNs to extend the length of f_tot to the length of
+            % Insert NaNs to extend the length of f_in to the length of
             % f_in
             nNaNs = length(f_obs) - length(f_int_s); 
             f_int = [f_int_s, nan(nNaNs, 1)'];
+
+            % Compute intrinsic frequency limits of integration
+            f_int_li = f_obs_li;
+            f_int_lf = f_obs_lf;
+            f_int_hi = f_obs_hi;
+            f_int_hf = f_obs_hf;
             
         end
     end 
     
-    %-- Compute Intrinsic Frequency Power Spectrum S(f_int) --%
+    %-- Compute Intrinsic 1D Spectrum S(f_in) --%
 
     % Compute f_int differentials via finite differencing
     df_int = diff(f_int);
@@ -210,8 +244,10 @@ function [S_fin, f_int, S_fob, f_obs, f_b, df_int, J, variance] = map_omni_dir_s
     end
 
     % Obtain indices for integration (non-NaN values at frequencies higher the 0.01 and 0.1 Hz)
-    idx_fob_l = ~isnan(S_fob) & (f_obs >= 0.01)'; idx_fin_l = ~isnan(S_fin) & (f_int >= 0.01)';
-    idx_fob_h = ~isnan(S_fob) & (f_obs >= 0.1)'; idx_fin_h = ~isnan(S_fin) & (f_int >= 0.1)';
+    idx_fob_l = ~isnan(S_fob) & (f_obs >= f_obs_li)' & (f_obs <= f_obs_lf)'; 
+    idx_fin_l = ~isnan(S_fin) & (f_int >= f_int_li)' & (f_int <= f_int_lf)';
+    idx_fob_h = ~isnan(S_fob) & (f_obs >= f_obs_hi)' & (f_obs <= f_obs_hf)'; 
+    idx_fin_h = ~isnan(S_fin) & (f_int >= f_int_hi)' & (f_int <= f_int_hf)';
 
     % Compute variance by interagting over two bands of the spectrum
     variance = [sum(S_fob(idx_fob_l)*df_obs),...
