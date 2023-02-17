e023a45f2b799e8914b0592c00a7d50b43dfb1a4
Luke Colosi <lcolosi@ucsd.edu>
2023-02-17 12:36:58 -0800

    Add notes on how mean and standard deviation of wind direction are computed.

--- tools/direction_stats.m
+++ tools/direction_stats.m
@@ -27,8 +27,10 @@ function [m_theta,std_theta, stdm_theta] = direction_stats(theta, dt, task)
     %   -----
     %   (1) The mean and standard deviation are computed using the 
     %       Yamartino Method. For more information, see:
+    %
     %           (a) Wiki page - https://en.wikipedia.org/wiki/Yamartino_method
     %           (b) Publication - https://urldefense.com/v3/__https://journals.ametsoc.org/view/journals/apme/23/9/1520-0450_1984_023_1362_acospe_2_0_co_2.xml?tab_body=pdf__;!!Mih3wA!AMtkrURmRZGuK8lwOc1Zx2ixE7Zqo3CSEC3tVgpEC2X6gC9HD4UJu49dcrL_QgeyJutdnXi-DAqbcNE$
+    %
     %%%%
     
     % Compute the cosine and sine components of the angle: 
